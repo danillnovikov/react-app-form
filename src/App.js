8fefd6e34da4993f94e31c2b8264e295ef4a42a4
Daniil <danill.novikov@mail.ru>
2022-06-11 17:09:09 +0300

    create path home

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/success" element={<Success />} />
